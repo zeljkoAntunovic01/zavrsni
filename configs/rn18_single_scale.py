9aca3dd1aba46533aebb7958dca30c4820fc68a6
ZeljkoAntunovic <za53008@fer.hr>
2023-06-02 11:05:57 +0200

    set num_epochs to 2 and val_epoch to 1 for testing purposes

--- configs/rn18_single_scale.py
+++ configs/rn18_single_scale.py
@@ -79,7 +79,7 @@ else:
     lr_min = 1e-6
     fine_tune_factor = 4
     weight_decay = 1e-4
-    epochs = 3 #used to be 250
+    epochs = 2 #used to be 250
 
     optim_params = [
         {'params': model.random_init_params(), 'lr': lr, 'weight_decay': weight_decay},
